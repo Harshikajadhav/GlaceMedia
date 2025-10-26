01e459a96491f3e9a2155ee9e1bec8272f60ef1e
Harshika Jadhav <71900830+Harshikajadhav@users.noreply.github.com>
2025-10-26 17:13:55 +0530

    Add files via upload

---
+++ try - Copy/footer.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Welcome to Glace Media</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="https://kit.fontawesome.com/2366c91d64.js" crossorigin="anonymous"></script>
+    <!-- Google Font Links starts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Montserrat:ital,wght@0,100..900;1,100..900&family=Oswald:wght@200..700&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=close" />
+    <!-- Google Font Links ends -->
+</head>
+<body>
+
+<div class="footer-content" style="display: flex; justify-content: center;"> 
+  <div class="footer-logo">_____________________________________________________________<img src="image/footer-logo.png" style="height: 70px; width: 300px; overflow: hidden;">_____________________________________________________________</div>
+</div><br><br>
+<div class="footer">
+  <div class="footer-logos">
+      <nav style="margin-bottom: 15px; display: flex; justify-content: space-evenly;">
+          <a href="index.php" style="font-size:20px; margin: 0 15px; text-decoration: none;">Home</a>
+          <a href="aboutus.php" style="font-size:20px; margin: 0 15px; text-decoration: none;">About us</a>
+          <a href="ourservices.php" style="font-size:20px; margin: 0 15px; text-decoration: none;">Services</a>
+          <a href="brands.php" style="font-size:20px; margin: 0 15px; text-decoration: none;">Work</a>
+          <a href="contactus.php" style="font-size:20px; margin: 0 15px; text-decoration: none;">Contact us</a>
+      </nav>
+  </div><br>
+  <div class="footer">
+      <div class="footer-logos">
+          <nav style="margin-bottom: 15px; display: flex; align-items: center; justify-content: center;">
+              <a href="#" style="color: #ffffff; margin: 0 50px; text-decoration: none;"><img src="image/2.png" style="height: 40px; width: 45px;"></a>
+              <a href="#" style="color: #ffffff; margin: 0 50px; text-decoration: none;"><img src="image/4.png" style="height: 33px; width: 25px;"></a>
+              <a href="#" style="color: #ffffff; margin: 0 50px; text-decoration: none;"><img src="image/3.png" style="height: 40px; width: 40px;"></a>
+              <a href="#" style="color: #ffffff; margin: 0 50px; text-decoration: none;"><img src="image/5.png" style="height: 30px; width: 30px;"></a>
+              <a href="#" style="color: #ffffff; margin: 0 50px; text-decoration: none;"><img src="image/1.png" style="height: 35px; width: 40px;"></a>
+          </nav>
+      </div><br>
+      <div class="copyright-names">© 2024 Glace. All rights reserved. </div>
+</div>
+    <script src="script.js"></script>
+</body>
+
+</html>
---
+++ try - Copy/index.php
@@ -0,0 +1,110 @@
+
+<html lang="en">
+
+<head>
+    <title>Welcome to Glace Media</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="https://kit.fontawesome.com/2366c91d64.js" crossorigin="anonymous"></script>
+    <!-- Google Font Links starts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Montserrat:ital,wght@0,100..900;1,100..900&family=Oswald:wght@200..700&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+        rel="stylesheet">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=close" />
+    <!-- Google Font Links ends -->
+</head>
+
+<body>
+    
+<?php 
+include 'header.php';
+?>
+    <!-- FOR PC -->
+<br><br><br>
+<div class="brands-list">
+    <!-- Row 1 -->
+    <div class="brand-list">
+        <div class="brand-box" data-image="image/work1/1.jpg"><img src="image/1.png"></div>
+        <div class="brand-box" data-image="image/work1/1.jpg"><img src="image/1.png"></div>
+        <div class="brand-box" data-image="image/work1/1.jpg"><img src="image/1.png"></div>
+        <div class="brand-box" data-image="image/work1/1.jpg"><img src="image/1.png"></div>
+    </div>
+
+    <!-- Placeholder for Hovered Image -->
+    <div class="image-placeholder">
+        <img src="" alt="Hovered Image" id="hovered-image">
+    </div>
+</div>
+<br><br>
+<div class="brands-list2">
+    <!-- Row 1 -->
+    <div class="brand-list2">
+        <div class="brand-box2" data-image="image/work1/1.jpg"><img src="image/1.png"></div>
+        <div class="brand-box2" data-image="image/work1/3.png"><img src="image/1.png"></div>
+        <div class="brand-box2" data-image="image/work1/4.jpg"><img src="image/1.png"></div>
+        <div class="brand-box2" data-image="image/work1/4.jpg"><img src="image/1.png"></div>
+    </div>
+
+    <!-- Placeholder for Hovered Image -->
+    <div class="image-placeholder2">
+        <img src="" alt="Hovered Image" id="hovered-image2">
+    </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- FOR MOBILE -->
+
+<div class="brands-list">
+    <!-- Row 1 -->
+    <div class="brand-list">
+        <div class="brand-box" data-image="image/work1/1.jpg"><img src="image/1.png"></div>
+        <div class="brand-box" data-image="image/work1/1.jpg"><img src="image/1.png"></div>
+        <div class="brand-box" data-image="image/work1/1.jpg"><img src="image/1.png"></div>
+        <div class="brand-box" data-image="image/work1/1.jpg"><img src="image/1.png"></div>
+    </div>
+
+    <!-- Placeholder for Hovered Image -->
+    <div class="image-placeholder">
+        <img src="" alt="Hovered Image" id="hovered-image">
+    </div>
+</div>
+<br><br>
+<div class="brands-list2">
+    <!-- Row 1 -->
+    <div class="brand-list2">
+        <div class="brand-box2" data-image="image/work1/1.jpg"><img src="image/1.png"></div>
+        <div class="brand-box2" data-image="image/work1/3.png"><img src="image/1.png"></div>
+        <div class="brand-box2" data-image="image/work1/4.jpg"><img src="image/1.png"></div>
+        <div class="brand-box2" data-image="image/work1/4.jpg"><img src="image/1.png"></div>
+    </div>
+
+    <!-- Placeholder for Hovered Image -->
+    <div class="image-placeholder2">
+        <img src="" alt="Hovered Image" id="hovered-image2">
+    </div>
+</div>
+
+
+    <!-- FOOTER STARTS -->
+<div class="max-spacer"></div>
+<?php
+    include 'footer.php';
+    ?>
+    <script src="script.js"></script>
+    <script src="brands.js"></script>
+</body>
+
+</html>
